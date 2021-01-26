02d37f794b23f1219a3126e961af807c836f6b1e
Alexander Ariza <66517893+Alexanderariza@users.noreply.github.com>
2021-01-27 00:34:56 +0100

    Update org-locations.js

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -5,7 +5,7 @@ var addressPoints = [
     -4.4481758
   ],
   [
-    "<h1>Casa Espanya, Bogota, CO, </h2><p><img>src=http://www.fao.org/3/y7220e/y7220e.jpg alt= width=50% height=50%</img><ul><li><a href=https://www.youtube.com/>listing-markers example</a></li><li><a href=https://www.youtube.com/>two-sidebars example</a></li></ul>La Casa Espanya, o Casa Llunell, és un casal gòtic de l'Hospitalet de Llobregat Barcelonès, declarat bé cultural d'interès nacional.</p>",
+    "<h1>Casa Espanya, </h2>Bogota, CO, <p> <img> src=http://www.fao.org/3/y7220e/y7220e.jpg width=50% height=50%</img><ul><li><a href=https://www.youtube.com/>listing-markers example</a></li><li><a href=https://www.youtube.com/>two-sidebars example</a></li></ul>La Casa Espanya, o Casa Llunell, és un casal gòtic de l'Hospitalet de Llobregat Barcelonès, declarat bé cultural d'interès nacional.</p>",
     4.6482837,
     -74.2478938
   ],
