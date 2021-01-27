49ff82fcb2ade9c9821d4979d89bbf9e18ebe0ae
Alexander Ariza <66517893+Alexanderariza@users.noreply.github.com>
2021-01-27 11:31:30 +0100

    Update org-locations.js

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -5,7 +5,7 @@ var addressPoints = [
     -4.4481758
   ],
   [
-    "<h1>Casa Espanya, </h2> <p> Bogota, CO, <img src=https://image.slidesharecdn.com/concepts7sep2015v2-161115214439/95/land-cover-classification-system-classification-concepts-1-638.jpg?cb=1479246957 style=width:100px;> <ul><li><a href=https://www.youtube.com/>listing-markers example</a></li><li><a href=https://www.youtube.com/>two-sidebars example</a></li></ul>La Casa Espanya, o Casa Llunell, és un casal gòtic de l'Hospitalet de Llobregat Barcelonès, declarat bé cultural d'interès nacional.</p>",
+    "<h1>Applying the Land Cover Classification System in Colombia, </h2> <p> Bogota, CO, <img src=https://image.slidesharecdn.com/concepts7sep2015v2-161115214439/95/land-cover-classification-system-classification-concepts-1-638.jpg?cb=1479246957 style=width:100px;> <ul><li><a href=https://www.researchgate.net/publication/282852020_METODOLOGIAS_BASADAS_EN_SISTEMAS_DE_CLASIFICACION_CORINE-FAO_EN_LOS_ESTUDIOS_DE_LA_COBERTURA_Y_USO_DE_LA_TIERRA_APOYADO_EN_GEOTECNOLOGIAS>listing-markers example</a></li></ul> A project for the adaptation of the FAO methodology LCCS - Land Cover Classification System, as an alternative classification system for natural resources management in Colombia.</p>",
     4.6482837,
     -74.2478938
   ],
