a4d4d68dbcf36c93c7994c6fabb407e0a223332f
Alexander Ariza <66517893+Alexanderariza@users.noreply.github.com>
2021-01-27 11:51:02 +0100

    Update org-locations.js

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -5,12 +5,12 @@ var addressPoints = [
     -4.4481758
   ],
   [
-    "<h1>Applying the Land Cover Classification System in Colombia, </h2> <p> Bogota, CO, <img src=https://un-spider.org/sites/default/files/130723_IGACLCCS.jpg style=width:100px;> <ul><li><a href=https://un-spider.org/fr/node/6757>LCCS project</a></li></ul> A project for the adaptation of the FAO methodology LCCS - Land Cover Classification System, as an alternative classification system for natural resources management in Colombia.</p>",
+    "<h1>Applying the Land Cover Classification System in Colombia </h2> <p> Bogota, CO, <img src=https://un-spider.org/sites/default/files/130723_IGACLCCS.jpg style=width:100px;> <ul><li><a href=https://un-spider.org/fr/node/6757>LCCS project</a></li></ul> A project for the adaptation of the FAO methodology LCCS - Land Cover Classification System, as an alternative classification system for natural resources management in Colombia.</p>",
     4.6482837,
     -74.2478938
   ],
   [
-    "<h1>Morphological characterization using Worldview-3 images , </h2> <p> Bogota, CO, <img src=https://un-spider.org/sites/default/files/130723_IGACLCCS.jpg style=width:100px;> <ul><li><a href=http://www.scielo.org.co/scielo.php?pid=S0123-37692018000100105&script=sci_arttext&tlng=en>Using spectral ratios derived from remote sensing to geomorphological surveys</a></li></ul> This work presents an alternative to the identification of such units in the landscape complex of the archipelago of San Andrés and Providencia, by using spectral indices from WorldView 2 and 3 satellite images.</p>",
+    "<h1>Morphological characterization using Worldview-3 images </h2> <p> Bogota, CO, <img src=https://un-spider.org/sites/default/files/130723_IGACLCCS.jpg style=width:100px;> <ul><li><a href=http://www.scielo.org.co/scielo.php?pid=S0123-37692018000100105&script=sci_arttext&tlng=en>Using spectral ratios derived from remote sensing to geomorphological surveys</a></li></ul> This work presents an alternative to the identification of such units in the landscape complex of the archipelago of San Andrés and Providencia, by using spectral indices from WorldView 2 and 3 satellite images.</p>",
     12.5481752,
     -81.7208727,17
   ],
